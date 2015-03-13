b4117b33abc00af54a53bb5bd04700ab3a923173
Kamil Piekutowski <kpieku2@uic.edu>
2015-03-13 00:18:49 -0500

    added uic logo bar and fixed the blue color

--- website/templates/header.php
+++ website/templates/header.php
@@ -17,6 +17,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <img src="img/uicbanner092809.png" width=110%%> 
   </head>
   <body>
 
